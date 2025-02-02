3967a43a408277854c1cc04cc814e14e80f1f269
hannah-l-b <hlbermudez@ou.edu>
2025-02-02 17:20:48 -0600

    Edited myread function

--- R/myread.R
+++ R/myread.R
@@ -1,6 +1,7 @@
 #' Read a CSV file
 #'
 #' @param csv A string specifying the CSV filename.
+#' @param dird A string specifying the directory where the CSV file is located.
 #'
 #' @returns A data frame containing the contents of the CSV file.
 #' @export
@@ -8,15 +9,6 @@
 #' @examples
 #' myread("data.csv")
 myread=function(csv){
-  # Define `dird` globally if not already defined
-  if (!exists("dird", envir = .GlobalEnv)) {
-    dird <<- getwd()  # Set default to current working directory
-    message("Global variable 'dird' was not set. Using working directory: ", dird)
-  }
-
-  # Construct file path
-  fl <- file.path(dird, csv)
-
-  # Read the CSV file
-  read.table(fl, header = TRUE, sep = ",")
+  fl=paste(dird,csv,sep="")
+  read.table(fl,header=TRUE,sep=",")
 }
