ba609259ac5de16401328e8d6d4c6e69118e8dec
hannah-l-b <hlbermudez@ou.edu>
2025-02-02 17:25:54 -0600

    Edited myread function

--- R/myread.R
+++ R/myread.R
@@ -1,14 +1,15 @@
 #' Read a CSV file
 #'
 #' @param csv A string specifying the CSV filename.
-#' @param dird A string specifying the directory where the CSV file is located.
+#' @param dir A string specifying the directory containing the CSV file.
 #'
 #' @returns A data frame containing the contents of the CSV file.
 #' @export
 #'
 #' @examples
 #' myread("data.csv")
-myread=function(csv){
-  fl=paste(dird,csv,sep="")
+#' myread("data.csv", dir = "/path/to/data/")
+myread=function(csv, dir = getwd()){
+  fl=paste(dir,csv,sep="")
   read.table(fl,header=TRUE,sep=",")
 }
