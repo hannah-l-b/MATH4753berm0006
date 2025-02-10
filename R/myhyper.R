c9d2cb024f41a9729c6e52f4b97ea877d848f1ab
hannah-l-b <hlbermudez@ou.edu>
2025-02-10 10:55:18 -0600

    Initial myhyper() function

---
+++ R/myhyper.R
@@ -0,0 +1,43 @@
+#' Hypergeometric Simulation
+#'
+#' @param iter Integer. Number of iterations.
+#' @param N Integer. Population size.
+#' @param r Integer. Number of successes in the population.
+#' @param n Integer. Number of draws.
+#' @param plot Logical. Whether to plot the results. Default is TRUE.
+#'
+#' @returns A table of proportions representing the frequency of the number of successes across all iterations.
+#' @export
+#'
+#' @examples
+#' #' # Run the simulation with default parameters
+#' myhyper()
+#'
+#' # Run the simulation with custom parameters and no plot
+#' myhyper(iter = 500, N = 50, r = 20, n = 10, plot = FALSE)
+#'
+#' # Run the simulation with fewer iterations and a smaller population
+#' myhyper(iter = 50, N = 10, r = 4, n = 3)
+myhyper=function(iter=100,N=20,r=12,n=5, plot = TRUE){
+  # make a matrix to hold the samples
+  #initially filled with NA's
+  sam.mat=matrix(NA,nr=n,nc=iter, byrow=TRUE)
+  #Make a vector to hold the number of successes over the trials
+  succ=c()
+  for( i in 1:iter){
+    #Fill each column with a new sample
+    sam.mat[,i]=sample(rep(c(1,0),c(r,N-r)),n,replace=FALSE)
+    #Calculate a statistic from the sample (this case it is the sum)
+    succ[i]=sum(sam.mat[,i])
+  }
+  #Make a table of successes
+  succ.tab=table(factor(succ,levels=0:n))
+  #Make a barplot of the proportions
+  if (plot == TRUE) {
+    barplot(succ.tab/(iter), col=rainbow(n+1), main="HYPERGEOMETRIC simulation", xlab="Number of successes")
+    succ.tab/iter
+  }
+
+  return(succ.tab / iter)
+
+}
