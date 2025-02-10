66dfdcd18166eaf6c858d7b4aae57649f2b7433e
hannah-l-b <hlbermudez@ou.edu>
2025-02-10 10:51:14 -0600

    Update mybin() function

--- R/mybin.R
+++ R/mybin.R
@@ -3,6 +3,7 @@
 #' @param iter Integer. The number of iterations/simulations to run. Default is 100.
 #' @param n Integer. The number of trials in each iteration. Default is 10.
 #' @param p Numeric. The probability of success in each trial. Default is 0.5.
+#' @param plot Logical. Whether to plot the results. Default is TRUE.
 #'
 #' @returns A table of proportions showing the frequency of each number of successes across all iterations as well as a bar plot.
 #' @export
@@ -10,7 +11,7 @@
 #' @examples
 #' mybin()
 #' mybin(iter = 1000, n = 10, p = 0.7)
-mybin=function(iter=100,n=10, p=0.5){
+mybin=function(iter=100,n=10, p=0.5, plot = TRUE){
   # make a matrix to hold the samples
   #initially filled with NA's
   sam.mat=matrix(NA,nr=n,nc=iter, byrow=TRUE)
@@ -25,6 +26,11 @@ mybin=function(iter=100,n=10, p=0.5){
   #Make a table of successes
   succ.tab=table(factor(succ,levels=0:n))
   #Make a barplot of the proportions
-  barplot(succ.tab/(iter), col=rainbow(n+1), main="Binomial simulation", xlab="Number of successes")
-  succ.tab/iter
+  if (plot == TRUE) {
+    barplot(succ.tab/(iter), col=rainbow(n+1), main="Binomial simulation", xlab="Number of successes")
+    succ.tab/iter
+  }
+
+  return(succ.tab / iter)
+
 }
